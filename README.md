67ca2a2b667a6f6b94f64fdc2898f8e9060abb3f
rpaducel <razvanrazvan225@gmail.com>
2024-04-23 22:36:03 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+https://react-portofolio-app-five.vercel.app/
+<img src='./src/assets/localhost_3000_.png.jpg'>
--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
             <div className="desktopMenu">
                <Link activeClass='active' to='home' spy={true} smooth={true} offset={-79} duration={500} className="desktopMenuListItem">Home</Link>
                <Link activeClass='active' to='about' spy={true} smooth={true} offset={-79} duration={500} className="desktopMenuListItem">About</Link>
-               <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-80} duration={500} className="desktopMenuListItem">Projects</Link>
+               <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-79} duration={500} className="desktopMenuListItem">Projects</Link>
                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-window.innerHeight + 330} duration={500} className="desktopMenuListItem">Contact</Link>
             </div>
             
